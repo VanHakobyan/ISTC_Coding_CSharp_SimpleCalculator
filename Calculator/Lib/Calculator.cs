9a9d9faf5e865c5793472d2bfdf7db3b995df06b
Arman <armpoloyan@gmail.com>
2018-10-22 19:25:59 +0400

    add exercize

--- Calculator/Lib/Calculator.cs
+++ Calculator/Lib/Calculator.cs
@@ -8,6 +8,18 @@ namespace Lib
 {
     public class Calculator
     {
+      
+      public bool GetSymtricNumber (ulong a)
+      {
+            string N = Convert.ToString(a);
+            for (int i = 0; i < N.Length; i++)
+            {
+                if (N[i] != N[N.Length - 1])
+                    return false;
+            }
+            return true;
+ 
+      }
 
     }
 }
