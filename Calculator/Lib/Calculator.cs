49e70c5fefa0fb7d5feae5b30c1a920b594ab2e4
levonaramyan <levonaramyan@gmail.com>
2018-10-22 19:16:15 +0400

    Added Task 14: A method, which returns the multiplication of even digits less than 7.

--- Calculator/Lib/Calculator.cs
+++ Calculator/Lib/Calculator.cs
@@ -8,6 +8,18 @@ namespace Lib
 {
     public class Calculator
     {
+        public int EvenDigitsMult(int num)
+        {
+            int mult = 1;
+            num = Math.Abs(num);
 
+            while (num > 0)
+            {
+                mult = (num % 2 == 0 && num % 10 > 0 && num % 10 < 7) ? num % 10 : mult;
+                num /= 10;
+            }
+
+            return (mult > 1)? mult : int.MinValue;
+        }
     }
 }
