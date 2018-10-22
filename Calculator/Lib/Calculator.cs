40a53f21c05a8dfd27b41575c8effb22f8f592a2
zoro83 <zohrab.karapetyan1983@gmail.com>
2018-10-22 19:38:58 +0400

    Return Minimum Odd Digit

--- Calculator/Lib/Calculator.cs
+++ Calculator/Lib/Calculator.cs
@@ -8,6 +8,23 @@ namespace Lib
 {
     public class Calculator
     {
+        public int MinOddDigit(int num)
+        {
+            int odd = 99;
+            bool flag = false;
 
+            while (num != 0)
+            {
+                int digit = (int)num % 10;
+                if (digit % 2 != 0 && digit < odd)
+                {
+                    odd = digit;
+                    flag = true;
+
+                }
+                num /= 10;
+            }
+            return (flag) ? odd : -1;
+        }
     }
 }
