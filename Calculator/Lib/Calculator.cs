1e1bdee0fcbca533e095e4636c2eabfa65f6fa5b
zbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-22 20:07:18 +0400

    Task 11 IsNumberDecrease

--- Calculator/Lib/Calculator.cs
+++ Calculator/Lib/Calculator.cs
@@ -8,6 +8,19 @@ namespace Lib
 {
     public class Calculator
     {
-
+        static bool IsNumberDecrease(int number)
+        {
+            int n = number, numCount = (int)Math.Log10(number);
+            while (n > 0 && numCount > 0)
+            {
+                int a = n / (int)Math.Pow(10, numCount);
+                int temp = n % (int)Math.Pow(10, numCount);
+                int b = temp / (int)Math.Pow(10, numCount - 1);
+                if (a <= b) return false;
+                n %= (int)Math.Pow(10, numCount);
+                numCount--;
+            }
+            return true;
+        }
     }
 }
