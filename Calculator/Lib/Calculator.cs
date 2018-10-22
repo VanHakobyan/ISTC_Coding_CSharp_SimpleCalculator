96cb025ba57ac68eba35d4555796ab5d1263b7ae
edvinhakobyan <edvin1984edvin@gmail.com>
2018-10-22 19:23:49 +0400

    add GetSimpleMultCount method

--- Calculator/Lib/Calculator.cs
+++ Calculator/Lib/Calculator.cs
@@ -8,6 +8,24 @@ namespace Lib
 {
     public class Calculator
     {
+        public int GetSimpleMultCount(int a)
+        {
+            int[] mem = new int[500];
+            int count = 0, i = 2;
+            while (a > 0)
+            {
+                if (a % i == 0 && !mem.Contains(i))
+                {
+                    mem[count] = i;
+                    count++;
+                }
+                while(a % i == 0)
+                    a /= i;
+
+                i++;
+            }
+            return count;
+        }
 
     }
 }
