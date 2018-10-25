6eecf18fa6d1dc44ba2308f4f53ac41cb19637be
Varduhi.Khachatryan <varduhi.khachatryan>
2018-10-25 15:56:59 +0400

    Some structural changes. Doesn't work properly

---
+++ Calculator/DesktopApp/Helpers/Helper.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace DesktopApp.Helpers
+{
+    public class Helper
+    {
+        #region "Public fields"
+        public List<Operations> TwoPosOperationsList { get; set; }
+        public List<Operations> OnePosOperationsList { get; set; }
+        #endregion
+
+        #region "ctor"
+
+        public Helper()
+        {
+            TwoPosOperationsList = new List<Operations>
+            {
+                new Operations
+                {
+                    Id = 9,
+                    Name = "Ամենամեծ ընհանուր բաժանարար",
+                    Description = "Հաշվում է 2 թվերի ամենամեծ ընհանուր բաժանարարը:",
+                    Type = OperationType.TwoPosOperation
+                },
+                new Operations
+                {
+                    Id = 10,
+                    Name = "Ամենափոքր ընհանուր բազմապատիկը",
+                    Description = "Հաշվում է 2 թվերի ամենափոքր ընհանուր բազմապատիկը։",
+                    Type = OperationType.TwoPosOperation
+                },
+                new Operations
+                {
+                    Id = 11,
+                    Name = "Միջին թվաբանական",
+                    Description = "Հաշվում է 2 թվերի միջին թվաբանականը։",
+                    Type = OperationType.TwoPosOperation
+                },
+                new Operations
+                {
+                    Id = 12,
+                    Name = "Միջին երկրաչափականը",
+                    Description = "Հաշվում է 2 թվերի միջին երկրաչափականը։",
+                    Type = OperationType.TwoPosOperation
+                }
+            };
+
+            OnePosOperationsList = new List<Operations>
+            {
+                new Operations
+                {
+                    Id = 1,
+                    Name = "4-ից մեծ կենտ թվանշանների գումար",
+                    Description = "Հաշվում է տրված թվի 4-ից մեծ կենտ թվանշանների գումարը։",
+                    Type = OperationType.OnePosOperation
+                },
+                new Operations
+                {
+                    Id = 2,
+                    Name = "Պարզ արտադրիչների քանակ",
+                    Description = "Հաշվում է տրված բնական թվի պարզ արտադրիչների քանակը",
+                    Type = OperationType.OnePosOperation
+                },
+                new Operations
+                {
+                    Id = 3,
+                    Name = "7-ից փոքր զույգ թվանշանների արտադրյալ",
+                    Description = "Հաշվում է տրված թվի 7-ից փոքր զույգ թվանշանների արտադրյալը։",
+                    Type = OperationType.OnePosOperation
+                },
+                new Operations
+                {
+                    Id = 4,
+                    Name = "Ստուգել կատարյալ լինելը",
+                    Description = "Պարզում է, արդյոք, տրված թիվը կատարյալ է։",
+                    Type = OperationType.OnePosOperation
+                },
+                new Operations
+                {
+                    Id = 5,
+                    Name = "Ստուգել սիմետրիկ լինելը",
+                    Description = "Պարզում է, արդյոք, տրված թիվը սիմետրիկ է։",
+                    Type = OperationType.OnePosOperation
+                },
+                new Operations
+                {
+                    Id = 6,
+                    Name = "Ստուգել պարզ լինելը",
+                    Description = "Ստուգում է, արդյոք, տրված թիվը պարզ է։",
+                    Type = OperationType.OnePosOperation
+                },
+                new Operations
+                {
+                    Id = 7,
+                    Name = "Ստուգել նվազող հաջ․ լինելը",
+                    Description = "Ստուգում է, տրված թվի թվանշանները ձախից աջ դիտարկելիս կազմում են, արդյոք, նվազող հաջորդականություն",
+                    Type = OperationType.OnePosOperation
+                },
+                new Operations
+                {
+                    Id = 8,
+                    Name = "Պարզել, որևէ թվի ֆակտորիալ լինելը",
+                    Description = "Պարզում է, հանդիսանում է, արդյոք, տրված թիվը որևէ թվի ֆակտորիալ։",
+                    Type = OperationType.OnePosOperation
+                },
+            };
+        }
+
+        #endregion
+
+        #region "Enums"
+
+        public enum OperationType
+        {
+            OnePosOperation = 1,
+            TwoPosOperation
+        }
+
+        public enum OperationsEnum
+        {
+            OddDigitSum = 1,
+            GetSimpleMultCount,
+            EvenDigitsMult,
+            IsPerfect,
+            IsSymtricNumber,
+            IsNumberPrime,
+            IsNumberDecrease,
+            Factorial,
+            BiggestCommonDiviser,
+            SmallestCommonMultiplier,
+            ArithmeticMean,
+            GeometricMean
+        }
+
+        #endregion "Enums"
+
+        #region "Validation"
+
+        internal bool NumberValidation(string text)
+        {
+            Regex regex = new Regex("[^0-9]+");
+
+            return regex.IsMatch(text);
+        }
+
+        #endregion "Validation"
+
+        #region "Public Methods"      
+
+        public static object Caller(String myclass, String mymethod, string param1, string param2, bool isTwoPosOperation = false)
+        {
+            // Get a type from the string 
+            Type type = GetTypeByName(myclass);
+            // Create an instance of that type
+            Object obj = Activator.CreateInstance(type);
+            // Retrieve the method you are looking for
+            MethodInfo methodInfo = type.GetMethod(mymethod);
+            // Invoke the method on the instance we created above
+            //methodInfo.Invoke(obj, null);
+
+            //Type Mytype = Type.GetType("parminfo");
+            //MethodBase Mymethodbase = Mytype.GetMethod("mymethod");
+            ParameterInfo[] myParams = methodInfo.GetParameters();
+
+
+
+            object returnValue = isTwoPosOperation ? Convert.ToInt32(methodInfo.Invoke(obj, new object[] { param1, param2 })) : Convert.ToInt32(methodInfo.Invoke(obj, new object[] { param1 }));
+
+            return returnValue;
+        }
+
+        public static Type GetTypeByName(string className)
+        {
+            Type returnVal = null;
+
+            foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                Type[] assemblyTypes = a.GetTypes();
+                for (int j = 0; j < assemblyTypes.Length; j++)
+                {
+                    if (assemblyTypes[j].Name == className)
+                    {
+                        returnVal = (assemblyTypes[j]);
+                    }
+                }
+            }
+
+            return returnVal;
+        }
+
+        #endregion
+    }
+}
--- Calculator/DesktopApp/MainWindow.xaml.cs
+++ Calculator/DesktopApp/MainWindow.xaml.cs
@@ -2,21 +2,10 @@
 using Lib;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static DesktopApp.Helpers.Helper;
 
 namespace DesktopApp
 {
@@ -26,181 +15,44 @@ namespace DesktopApp
     public partial class MainWindow : Window
     {
         #region "Private fields"
-        private List<Operations> TwoPosOperationsList { get; set; }
-        private List<Operations> OnePosOperationsList { get; set; }
+        private Helper _helper;        
         #endregion "Private fields"
 
         #region "ctor"
         public MainWindow()
         {
             InitializeComponent();
-            //this is now local, some day will come from db (data base)
-            GetOperationsInfo();
+            Calculator _calc = new Calculator();
+            _helper = new Helper();
 
             //Bind Two positional operations (to see in dropdown)
-            TwoPosOperations.ItemsSource = TwoPosOperationsList;
+            TwoPosOperations.ItemsSource = _helper.TwoPosOperationsList;
             TwoPosOperations.SelectedValuePath = "Id";
             TwoPosOperations.DisplayMemberPath = "Name";
 
             //Bind One positional operations (to see in dropdown)
-            OnePosOperations.ItemsSource = OnePosOperationsList;
+            OnePosOperations.ItemsSource = _helper.OnePosOperationsList;
             OnePosOperations.SelectedValuePath = "Id";
             OnePosOperations.DisplayMemberPath = "Name";
 
         }
         #endregion "ctor"
-
-        #region "Helpers"
-        
+              
         //Prevent user add text in inputs
-        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        public void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-            Regex regex = new Regex("[^0-9]+");
-            e.Handled = regex.IsMatch(e.Text);
-        }
+            e.Handled = _helper.NumberValidation(e.Text);
+        }        
 
-        private static object Caller(String myclass, String mymethod, double param1, double param2, bool isTwoPosOperation = false)
-        {
-            // Get a type from the string 
-            Type type = GetTypeByName(myclass);
-            // Create an instance of that type
-            Object obj = Activator.CreateInstance(type);
-            // Retrieve the method you are looking for
-            MethodInfo methodInfo = type.GetMethod(mymethod);
-            // Invoke the method on the instance we created above
-            //methodInfo.Invoke(obj, null);
-            object returnValue = isTwoPosOperation ? Convert.ToInt32(methodInfo.Invoke(obj, new object[] { param1, param2 })) : Convert.ToInt32(methodInfo.Invoke(obj, new object[] { param1 }));
-
-            return returnValue;
-        }
-
-        private static Type GetTypeByName(string className)
-        {
-            Type returnVal = null;
-
-            foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                Type[] assemblyTypes = a.GetTypes();
-                for (int j = 0; j < assemblyTypes.Length; j++)
-                {
-                    if (assemblyTypes[j].Name == className)
-                    {
-                        returnVal = (assemblyTypes[j]);
-                    }
-                }
-            }
-
-            return returnVal;
-        }
-
-        //Just manually add info
-        private void GetOperationsInfo()
-        {
-            TwoPosOperationsList = new List<Operations>
-            {
-                new Operations
-                {
-                    Id = 9,
-                    Name = "Ամենամեծ ընհանուր բաժանարար",
-                    Description = "Հաշվում է 2 թվերի ամենամեծ ընհանուր բաժանարարը:",
-                    Type = OperationType.TwoPosOperation
-                },
-                new Operations
-                {
-                    Id = 10,
-                    Name = "Ամենափոքր ընհանուր բազմապատիկը",
-                    Description = "Հաշվում է 2 թվերի ամենափոքր ընհանուր բազմապատիկը։",
-                    Type = OperationType.TwoPosOperation
-                },
-                new Operations
-                {
-                    Id = 11,
-                    Name = "Միջին թվաբանական",
-                    Description = "Հաշվում է 2 թվերի միջին թվաբանականը։",
-                    Type = OperationType.TwoPosOperation
-                },
-                new Operations
-                {
-                    Id = 12,
-                    Name = "Միջին երկրաչափականը",
-                    Description = "Հաշվում է 2 թվերի միջին երկրաչափականը։",
-                    Type = OperationType.TwoPosOperation
-                }
-            };
-
-            OnePosOperationsList = new List<Operations>
-            {
-                new Operations
-                {
-                    Id = 1,
-                    Name = "4-ից մեծ կենտ թվանշանների գումար",
-                    Description = "Հաշվում է տրված թվի 4-ից մեծ կենտ թվանշանների գումարը։",
-                    Type = OperationType.OnePosOperation
-                },
-                new Operations
-                {
-                    Id = 2,
-                    Name = "Պարզ արտադրիչների քանակ",
-                    Description = "Հաշվում է տրված բնական թվի պարզ արտադրիչների քանակը",
-                    Type = OperationType.OnePosOperation
-                },
-                new Operations
-                {
-                    Id = 3,
-                    Name = "7-ից փոքր զույգ թվանշանների արտադրյալ",
-                    Description = "Հաշվում է տրված թվի 7-ից փոքր զույգ թվանշանների արտադրյալը։",
-                    Type = OperationType.OnePosOperation
-                },
-                new Operations
-                {
-                    Id = 4,
-                    Name = "Ստուգել կատարյալ լինելը",
-                    Description = "Պարզում է, արդյոք, տրված թիվը կատարյալ է։",
-                    Type = OperationType.OnePosOperation
-                },
-                new Operations
-                {
-                    Id = 5,
-                    Name = "Ստուգել սիմետրիկ լինելը",
-                    Description = "Պարզում է, արդյոք, տրված թիվը սիմետրիկ է։",
-                    Type = OperationType.OnePosOperation
-                },
-                new Operations
-                {
-                    Id = 6,
-                    Name = "Ստուգել պարզ լինելը",
-                    Description = "Ստուգում է, արդյոք, տրված թիվը պարզ է։",
-                    Type = OperationType.OnePosOperation
-                },
-                new Operations
-                {
-                    Id = 7,
-                    Name = "Ստուգել նվազող հաջ․ լինելը",
-                    Description = "Ստուգում է, տրված թվի թվանշանները ձախից աջ դիտարկելիս կազմում են, արդյոք, նվազող հաջորդականություն",
-                    Type = OperationType.OnePosOperation
-                },
-                new Operations
-                {
-                    Id = 8,
-                    Name = "Պարզել, որևէ թվի ֆակտորիալ լինելը",
-                    Description = "Պարզում է, հանդիսանում է, արդյոք, տրված թիվը որևէ թվի ֆակտորիալ։",
-                    Type = OperationType.OnePosOperation
-                },
-            };
-
-        }
-
-        #endregion "Helpers"
-
-        #region "Calculations"
+       #region "Calculations"
 
         private void TwoPosCalculate_Click(object sender, RoutedEventArgs e)
         {
             if (twoPosFrstNum.Text != "" && twoPosScndNum.Text != "" &&
                 TwoPosOperations.SelectedItem != null)
             {
-                var frstNum = Convert.ToDouble(twoPosFrstNum.Text);
-                var scndNum = Convert.ToDouble(twoPosScndNum.Text);
+                var frstNum = twoPosFrstNum.Text;
+                var scndNum = twoPosScndNum.Text;
 
                 var selectedItem = (Operations)TwoPosOperations.SelectedItem;
                 var result = Caller("Calculator", ((OperationsEnum)(selectedItem.Id)).ToString(), frstNum, scndNum, true);
@@ -214,10 +66,10 @@ namespace DesktopApp
         {
             if (onePosNum.Text != "" && OnePosOperations.SelectedItem != null)
             {
-                var num = Convert.ToDouble(onePosNum.Text);
+                var number = onePosNum.Text;
 
                 var selectedItem = (Operations)OnePosOperations.SelectedItem;
-                var result = Caller("Calculator", ((OperationsEnum)(selectedItem.Id)).ToString(), num, 0);
+                var result = Caller("Calculator", ((OperationsEnum)(selectedItem.Id)).ToString(), number, "");
 
                 OnePosResult.Text = result.ToString();
             }
