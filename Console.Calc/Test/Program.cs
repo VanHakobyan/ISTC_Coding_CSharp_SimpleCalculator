7f9613e14aed7d1a24d8b7166fbda38a27795f60
Ani Tumanyan <ani.tumanyan1998@mail.ru>
2019-06-28 21:09:57 +0400

    ex14

--- Console.Calc/Test/Program.cs
+++ Console.Calc/Test/Program.cs
@@ -11,14 +11,18 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            bool c=Ex5.Comparis(6);
-            //bool b = Ex7.CheckTheNumber(8);
-            //Console.WriteLine(b);
-            //int b = Ex4.MinBazm(2,4);
-            Console.WriteLine(c);
+            //bool c=Ex5.Comparis(6);
+            ////bool b = Ex7.CheckTheNumber(8);
+            ////Console.WriteLine(b);
+            ////int b = Ex4.MinBazm(2,4);
+            //Console.WriteLine(c);
             
-            int b = Ex4.MinBazm(2,4);
-            Console.WriteLine(b);
+            //int b = Ex4.MinBazm(2,5);
+            //Console.WriteLine(b);
+
+            int m = Ex14.Mul(12426);
+            Console.WriteLine(m);
+            Console.ReadKey();
         }
     }
 }
