b784f6b570bcec7d3ff59e2591d1bd38401584f2
tamara.vardanyan998@gmail.com <tamara.vardanyan998@gmail.com>
2019-06-28 20:41:52 +0400

    ex.13

--- Console.Calc/Logic/Ex13.cs
+++ Console.Calc/Logic/Ex13.cs
@@ -8,10 +8,19 @@ namespace Logic
 {
     public class Ex13
     {
-        public static void Sum(int n)
+        public static int Sum(int n)
         {
             //13.	Հաշվել տրված թվի 4-ից մեծ կենտ թվանշանների գումարը։
-            
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int s = n % 10;
+                if (s % 2 != 0 && s > 4)
+                   sum += s;
+                n /= 10;
+
+            }
+            return sum;
         }
     }
 }
--- Console.Calc/Test/Program.cs
+++ Console.Calc/Test/Program.cs
@@ -13,9 +13,10 @@ namespace Test
         {
             bool b = Ex7.CheckTheNumber(8);
             Console.WriteLine(b);
-            int b = Ex4.MinBazm(2,4);
+            int h = Ex4.MinBazm(2,4);
             Console.WriteLine(b);
-            int b=  Ex3.MaxBaj(8,12);
+            int c=  Ex3.MaxBaj(8,12);
+            int k = Ex13.Sum(2594);
             Console.WriteLine(b);
         }
     }
