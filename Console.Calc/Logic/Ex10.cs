435c2322ad35ad1dba530800ee1d55cbe917bb38
Lusinkhachatryan <lusine.khachatryan@armsoft.am>
2019-06-28 19:58:13 +0400

    Ex10

--- Console.Calc/Logic/Ex10.cs
+++ Console.Calc/Logic/Ex10.cs
@@ -8,5 +8,23 @@ namespace Logic
 {
     public class Ex10
     {
+
+        public static int QntS(uint n)
+        {
+            int qnt = 0;
+            int simpQnt = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    for (int j = 1; j <= i; i++)
+                    {
+                        if (i % j == 0)
+                            qnt += 1;
+                    }
+                if (qnt == 2)
+                    simpQnt += 1;
+            }
+            return simpQnt;
+        }
     }
 }
