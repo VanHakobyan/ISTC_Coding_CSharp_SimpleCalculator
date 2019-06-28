478504fc4196afeac09ca90696b538b17b8cb5d8
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-06-28 19:54:19 +0400

    MinMultiple IsSymmetrical

--- Console.Calc/Logic/Ex4.cs
+++ Console.Calc/Logic/Ex4.cs
@@ -10,15 +10,13 @@ namespace Logic
     {
         // 4.Գրել մեթոդ, որը հաշվում է 2 թվերի ամենափոքր ընհանուր բազմապատիկը:
 
-        public int MinMultiple(int n1, int n2)
+        static int MinMultiple(int n1, int n2)
         {
-
             while (n1 % n2 != 0)
             {
                 n1++;
             }
-            return n1;
-            
+            return n1;   
         }
     }
 }
