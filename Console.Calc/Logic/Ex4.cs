52bde31a402ec7560d3e087f9bda84214f4164e8
Ani Tumanyan <ani.tumanyan1998@mail.ru>
2019-06-28 19:59:32 +0400

    Ex4

--- Console.Calc/Logic/Ex4.cs
+++ Console.Calc/Logic/Ex4.cs
@@ -8,5 +8,23 @@ namespace Logic
 {
     public class Ex4
     {
+        //Գրել մեթոդ, որը հաշվում է 2 թվերի ամենափոքր ընհանուր բազմապատիկը:
+    
+       public static void minBazm(int a, int b)
+    {
+        for (int i = Math.Min(a,b); i<=a*b;i++)
+        {
+            
+            if(i%a==0 && i%b==0)
+            {
+                    return i;
+            }
+
+        }
+
     }
+        
+    }
+
+   
 }
